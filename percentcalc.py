5542cd85eb36dbf9f0accbc16b562b264a64d1e9
= <ka.bir.kalra1@gmail.com>
2017-10-04 19:34:00 -0400

    First Commit

---
+++ percentcalc.py
@@ -0,0 +1,13 @@
+amtnums = input("How many numbers are you entering?")
+totalnum = input("What is the total of the numbers?")
+intamtnums = int(amtnums)
+inttotalnum = int(totalnum)
+file = open("Percentages.txt", "w")
+for i in range(0, intamtnums):
+	num = input("Enter number")
+	percentage = float((1. * num/inttotalnum) * 100)
+	roundedpercentage = round(percentage,3)
+	file.write("Percentage of " + str(num) + " = " +str(roundedpercentage)+ "% \n")
+file.write("Total: " + str(totalnum))	
+
+
