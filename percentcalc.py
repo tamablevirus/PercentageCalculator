571fd28a35bdbe91da23b330dd1b076ccfa61bbf
= <ka.bir.kalra1@gmail.com>
2017-10-04 22:38:07 -0400

    Now you can choose digits to round to!

--- percentcalc.py
+++ percentcalc.py
@@ -1,12 +1,13 @@
 amtnums = input("How many numbers are you entering?")
 totalnum = input("What is the total of the numbers?")
+diground = input("How many digits would you like to round to?")
 intamtnums = int(amtnums)
 inttotalnum = int(totalnum)
 file = open("Percentages.txt", "w")
 for i in range(0, intamtnums):
 	num = input("Enter number")
 	percentage = float((1. * num/inttotalnum) * 100)
-	roundedpercentage = round(percentage,2)
+	roundedpercentage = round(percentage,diground)
 	file.write("Percentage of " + str(num) + " = " +str(roundedpercentage)+ "% \n")
 file.write("Total: " + str(totalnum))	
 
